e6456b6f0ba996dd8eb42d13bded898627ee786b
Andreas Koepke <andreas.koepke@businessnews.com.au>
2021-10-15 09:02:56 +0800

    Raw URL output of SoundCloud field value

---
+++ src/Plugin/Field/FieldFormatter/SoundCloudURLFormatter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\soundcloudfield\Plugin\Field\FieldFormatter;
+
+use Drupal\Component\Utility\Unicode;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'soundcloud_url' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "soundcloud_url",
+ *   module = "soundcloudfield",
+ *   label = @Translation("Raw output of SoundCloud URI"),
+ *   field_types = {
+ *     "soundcloud"
+ *   }
+ * )
+ */
+class SoundCloudURLFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = array();
+
+    foreach ($items as $delta => $item) {
+      if (!$item->isEmpty()) {
+        $elements[$delta]['#markup'] = $item->url;
+      }
+    }
+
+    return $elements;
+  }
+
+}
