3beee2fc5aa178ec8c486f0d507de8d2aedd56bc
Andreas Koepke <andreas.koepke@businessnews.com.au>
2021-10-18 16:42:43 +0800

    Workaround for color attribute not being set in iframe src. SoundCloud SDK is not sending through all options.

--- js/soundcloudfield.js
+++ js/soundcloudfield.js
@@ -17,16 +17,15 @@
           maxheight: settings.maxheight,
           show_artwork: settings.showartwork,
           show_playcount: settings.showplaycount,
-          color: settings.color
         };
 
         $('#' + settings.id, context).once('soundcloudfield').each(function() {
-          console.log(embedSettings);
           SC.oEmbed(trackUrl, embedSettings).then(function(oEmbed){
             var $markup = $('<div>' + oEmbed.html + '</div>');
-            $markup.find('iframe').height(settings.maxheight + 'px');
+            var $iframe = $markup.find('iframe');
+            $iframe.height(settings.maxheight + 'px');
+            $iframe.attr('src', $iframe.attr('src') + '&amp;color=%23' + settings.color);
             $('#' + settings.id, context).html($markup.html());
-            console.log($markup.html());
           });
         });
       });
