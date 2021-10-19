ed9637ffce31b81170d5c35855a54c8fd3d9b4d4
Andreas Koepke <andreas.koepke@businessnews.com.au>
2021-10-19 10:17:21 +0800

    Backport of changes to Javascript based embed  - Use Drupal settings and get rid of inline JS  - Support all player options for JS as well as PHP
    JS Embed does not work in Views at the moment

--- templates/js_embed.tpl.php
+++ templates/js_embed.tpl.php
@@ -1,18 +1,5 @@
 <div class="soundcloudfield-js-embed-wrapper">
-  <div id="<?php echo $id; ?>">
+  <div id="<?php echo $target; ?>">
 
   </div>
-  <script>
-    var track_url = '<?php echo $url; ?>';
-    var settings = {
-      auto_play: <?php echo $autoplay; ?>,
-      maxheight: '<?php echo $maxheight; ?>',
-      show_artwork: <?php echo $showartwork; ?>,
-      show_playcount: <?php echo $showplaycount; ?>,
-      color: '#<?php echo $color; ?>'
-    };
-
-    SC.oEmbed(track_url, settings).then(function(oEmbed){
-    document.getElementById('<?php echo $id; ?>').innerHTML=oEmbed.html});
-  </script>
 </div>
